e592103e93060199023d75697b4558986023c3ee
Yghor Lopes <yghorlopes6@gmail.com>
2023-12-26 17:29:14 -0300

    ajustes

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #052d55;
   margin-top: 0px;
 }
--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-light bg-light">
     <div class="container">
-      <span class="navbar-brand mb-0 h1">Monte o seu Lanche</span>
+      <span class="navbar-brand mb-0 h1">Combo Lanche</span>
     </div>
   </nav>
 </template>
